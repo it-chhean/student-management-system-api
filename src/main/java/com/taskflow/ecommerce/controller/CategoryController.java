182d2ef1d2bd22185092b99e5445b2664b3511e5
Chhoung Bunchhean <bchhean8@gmail.com>
2026-04-12 21:42:51 +0700

    update: Complete to setup Dockerfile compose.yml application.yml

--- src/main/java/com/taskflow/ecommerce/controller/CategoryController.java
+++ src/main/java/com/taskflow/ecommerce/controller/CategoryController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/api/categories/")
+@RequestMapping(path = "/api/categories")
 public class CategoryController {
 
     @GetMapping
