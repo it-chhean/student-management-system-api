41d848696413ee6fd582efab641569ae42334c0f
Chhoung Bunchhean <bchhean8@gmail.com>
2026-05-28 15:44:46 +0700

    Updated

--- src/main/java/com/taskflow/studentmanagement/security/jwt/JwtService.java
+++ src/main/java/com/taskflow/studentmanagement/security/jwt/JwtService.java
@@ -1,4 +1,12 @@
 package com.taskflow.studentmanagement.security.jwt;
 
-public class JwtService {
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface JwtService {
+
+    String extractUserName(String token);
+    String generateToken(UserDetails userDetails);
+    boolean isTokenValid(String token, UserDetails userDetails);
+
 }
+
--- src/main/java/com/taskflow/studentmanagement/security/service/impl/JwtServiceImpl.java
+++ src/main/java/com/taskflow/studentmanagement/security/service/impl/JwtServiceImpl.java
@@ -1,5 +1,76 @@
 package com.taskflow.studentmanagement.security.service.impl;
 
-public class JwtServiceImpl {
-      
+import com.taskflow.studentmanagement.security.jwt.JwtService;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.jspecify.annotations.NullMarked;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.MalformedParameterizedTypeException;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtServiceImpl implements JwtService {
+
+    private String jwtSiginKey;
+    private int EXPIRATION_TOKEN;
+
+    @Override
+    public String extractUserName(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    @Override
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    @Override
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUserName(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimResolvers) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolvers.apply(claims);
+    }
+
+    private String generatToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        try {
+            return Jwts.builder()
+                    .setClaims(extraClaims)
+                    .setSubject(userDetails.getUsername())
+                    .setIssuedAt(new Date(System.currentTimeMillis()))
+                    .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TOKEN))
+                    .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                    .compact();
+        }catch (Exception e) {
+            throw new JwtException("Error in generate token: [" + e.getMessage() + " ]");
+        }
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(getSigningKey()).build().parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSiginKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
